5d3d4af2aaa7cf864e19892982f38ff6c512f6f2
Carson Farmer <carson.farmer@gmail.com>
2021-01-21 10:58:43 -0800

    feat: wire up error message

--- src/containers/SignUpForm.tsx
+++ src/containers/SignUpForm.tsx
@@ -8,7 +8,7 @@ import Loader from "../components/Loader";
 import MessageBox from "../components/MessageBox";
 
 const SignUpForm = () => {
-  const [, actions] = useContext(Context);
+  const [state, actions] = useContext(Context);
   const [username, setUsername] = useState<string | undefined>();
   const [loading, setLoading] = useState<boolean | false>();
   const [email, setEmail] = useState<string | undefined>();
@@ -20,7 +20,10 @@ const SignUpForm = () => {
         e.preventDefault();
         setLoading(true);
         if (username && email)
-          actions.signUp(username, email, () => history.push("/success"));
+          actions.signUp(username, email, () => {
+            actions.clearError();
+            history.push("/success");
+          });
       }}
     >
       <h1>Create your Account</h1>
@@ -28,11 +31,9 @@ const SignUpForm = () => {
         Create encrypted, resilient, and cross-application data storage in
         seconds.
       </p>
-      <MessageBox
-        type="error"
-        message="The email is already registered"
-        title="Error"
-      />
+      {state.error && (
+        <MessageBox type="error" message={state.error} title="Error" />
+      )}
       <hr />
       <FormInput
         name="userEmail"
@@ -53,7 +54,9 @@ const SignUpForm = () => {
           <Loader size={60} count={2} speed={2} />
           <div>
             <p className="announcement" style={{ marginLeft: "20px" }}>
-              Creating Account... Please Wait
+              Please check your email...
+              <br />
+              you should have a confirmation link
             </p>
           </div>
         </div>
