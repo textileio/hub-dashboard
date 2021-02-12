4f72556740413921825432120c5cb5da893fc1fa
Carson Farmer <carson.farmer@gmail.com>
2021-02-12 14:15:35 -0800

    Production vs dev env vars (#31)
    * feat: setup defaults for dev and prod + admin updates
    
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>
    
    * feat: env var for stating
    
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- src/store/Textile.ts
+++ src/store/Textile.ts
@@ -5,7 +5,14 @@ import dotenv from "dotenv";
 // Dot env config setup for testing purposes only
 dotenv.config();
 
-// Return an admin client connected to localhost.
-const admin = new Admin(new Context(process.env.REACT_APP_HUB_HOST));
+const HUB_HOST =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3007"
+    : "https://webapi.hub.textile.io";
+
+// Return an admin client connected to prod, staging, or custom (via .env)
+const admin = new Admin(
+  new Context(process.env.REACT_APP_HUB_HOST ?? HUB_HOST)
+);
 
 export { admin, Context };
