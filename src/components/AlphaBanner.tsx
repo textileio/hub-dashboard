5735e0878c050cab0421b6813e6031f0d04efce5
Carson Farmer <carson.farmer@gmail.com>
2021-04-01 10:27:12 -0700

    chore: add alpha warning banner
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

---
+++ src/components/AlphaBanner.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { borderRadius } from "../utils";
+import { Beaker } from "@styled-icons/heroicons-solid/";
+
+const BannerContainer = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  padding: 10px 20px;
+  font-size: 1em;
+  background-color: ${({ theme }) => theme.neutral300};
+  border-bottom: 1px solid ${({ theme }) => theme.neutral300};
+  z-index: 100;
+`;
+
+const HeaderMenu = styled.span`
+  margin: auto auto;
+  display: flex;
+  color: ${({ theme }) => theme.primary};
+  transition: all 0.2s linear;
+  svg {
+    margin-right: 4px;
+    max-height: 20px;
+    max-width: 20px;
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Banner = () => (
+  <BannerContainer>
+    <HeaderMenu>
+      <Beaker />
+      The Hub Dashboard is an Alpha experiment. Expect bugs, missing features,
+      breaking changes, and issues... but have fun!
+    </HeaderMenu>
+  </BannerContainer>
+);
+
+export default Banner;
